f60ad63e72535654c0e9650c27713166bc29a75e
Manuel Nuñez <manoplamn@gmail.com>
2025-02-05 12:04:38 -0300

    Prueba de migracion de ramas

--- javascript/Alerta_register.js
+++ javascript/Alerta_register.js
@@ -1,5 +1,6 @@
 function MostrarAlerta() {
     window.alert('¡Esto es una appweb para pruebas, recuerde que sus datos no quedarán expuestos a nadie!')
     window.alert('Si lo desea puede utilizar datos falsos para utilizar la aplicación.')
+    //test
 }
 MostrarAlerta()
